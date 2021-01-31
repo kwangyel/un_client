47d1604c5131470fa42564fa1711b98dc7460503
kwangyel <kwangyel@gmail.com>
2021-02-01 02:53:45 +0600

    location marker and bommtom sheets

--- app/src/main/java/com/kwangyel/u_ride/MapActivity.java
+++ app/src/main/java/com/kwangyel/u_ride/MapActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
-import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.util.MapTileIndex;
@@ -22,12 +25,14 @@ public class MapActivity extends AppCompatActivity {
     private MapView mapView;
     private IMapController mapController;
     private BottomSheetBehavior bottomSheetBehavior;
+    private TextInputLayout destField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ConstraintLayout constraintLayout = findViewById(R.id.bottomSheet);
+        destField = findViewById(R.id.destLocation);
 
         mapView = findViewById(R.id.mapview);
         bottomSheetBehavior = BottomSheetBehavior.from(constraintLayout);
@@ -54,6 +59,33 @@ public class MapActivity extends AppCompatActivity {
         mapController.setZoom(15.0);
         mapController.setCenter(new GeoPoint(27.4712, 89.6339));
 
+        bottomSheetBehavior.setHideable(false);
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setFitToContents(false);
+        bottomSheetBehavior.setHalfExpandedRatio(0.4f);
+
+        final MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(10)
+                .setInputMode(MaterialTimePicker.INPUT_MODE_KEYBOARD)
+                .setTitleText("Select Pickup Time")
+                .build();
+        timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String hour = String.valueOf(timePicker.getHour());
+                String min = String.valueOf(timePicker.getMinute());
+                destField.setSuffixText(hour + " : "+min);
+            }
+        });
+
+        destField.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("kinley","Pressed clock");
+                timePicker.show(getSupportFragmentManager(),null);
+            }
+        });
     }
 }
